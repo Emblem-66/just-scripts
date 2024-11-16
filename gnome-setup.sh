5f0287a7e0de68f7002a8eb50e92eabbd6bb88ec
Emblem-66 <143352594+Emblem-66@users.noreply.github.com>
2024-11-16 18:54:27 +0100

    Update gnome-setup.sh

--- gnome-setup.sh
+++ gnome-setup.sh
@@ -7,7 +7,7 @@ dconf reset -f /
 flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 gsettings set org.gnome.mutter center-new-windows true
 gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
-gsettings set org.gnome.shell enabled-extensions ['caffeine@patapon.info', 'legacyschemeautoswitcher@joshimukul29.gmail.com']
+gsettings set org.gnome.shell enabled-extensions ['caffeine@patapon.info', 'legacyschemeautoswitcher@joshimukul29.gmail.com', 'tailscale@joaophi.github.com']
 gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
 gsettings set org.gnome.nautilus.preferences default-sort-order 'type'
 gsettings set org.gnome.nautilus.preferences default-sort-order 'name'
